6b895d555cc8771c1a944ea7d36fcd15fb6998f6
Morry <hm452317@gmail.com>
2024-12-24 17:30:01 +0900

    Add some comments

--- file_manipulator.py
+++ file_manipulator.py
@@ -1,10 +1,13 @@
 import sys
 
 def display_message():
-    # Write a help message about this program.
-    print("Write it later.")
+    print('Please enter one of the following commands to manipulate your file: reverse, copy, duplicate-contents, and replace-string')
+    print('If you enter "python3 file_manipulator.py reverse <inputpath> <outputpath>", you can reverse the contents of inputpath and then write them to outputpath.')
+    print('If you enter "python3 file_manipulator.py copy <inputpath> <outputpath>", you can copy the contents of inputpath to outputpath.')
+    print('If you enter "python3 file_manipulator.py duplicate-contents <inputpath> n", you can duplicate the contents of inputpath n times.')
+    print('If you enter "python3 file.manipulator.py replace-string <inputpath> needle newstring", you can replace needle with newstirng in inputpath.')
 
-if len(sys.argv) < 4:
+if len(sys.argv) < 4:  # Check whether the command is long enough to exexute the program.
     display_message()
     sys.exit(1)
 else:
@@ -17,7 +20,7 @@ else:
 
         with open(input_path) as f:
             content = f.read()
-            content = content[::1]
+            content = content[::1]  # Reverse the content
 
         with open(output_path, 'w') as f:
             f.write(content)
@@ -34,7 +37,7 @@ else:
     elif command == 'duplicate_contents':
         number_of_copies = sys.argv[3]
 
-        if not number_of_copies.isdecimal():
+        if not number_of_copies.isdecimal():  # Check whether the user inputs the number to duplicate the contents
             print('Please enter a natural number after the input path')
             sys.exit(1)
         
